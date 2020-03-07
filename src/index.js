5ca7e6919dfc9904a091cc8aa329cc798a3f833a
pray-lee <praycis@gmail.com>
2020-03-07 18:10:30 +0800

    加中文标志

--- src/index.js
+++ src/index.js
@@ -7,26 +7,31 @@ import {
     Switch,
     Redirect
 } from "react-router-dom";
+// 设置中文
+import zhCN from 'antd/es/locale/zh_CN';
+import { ConfigProvider } from 'antd'
 import { mainRoutes } from "./routes";
 
 ReactDOM.render(
-    <Router>
-        <Switch>
-            <Route path="/admin" render={routeProps=>{
-                // 做登录权限控制
-                return <App {...routeProps}/>
-            }} />
-            {
-                mainRoutes.map(item => {
-                    return (
-                        <Route key={item.pathname} component={item.component} path={item.pathname}></Route>
-                    )
-                })
-            }
-            <Redirect to="/admin" from="/" exact />
-            <Redirect to="/404" />
-        </Switch>
-    </Router>,
+    <ConfigProvider local={zhCN}>
+        <Router>
+            <Switch>
+                <Route path="/admin" render={routeProps=>{
+                    // 做登录权限控制
+                    return <App {...routeProps}/>
+                }} />
+                {
+                    mainRoutes.map(item => {
+                        return (
+                            <Route key={item.pathname} component={item.component} path={item.pathname}></Route>
+                        )
+                    })
+                }
+                <Redirect to="/admin" from="/" exact />
+                <Redirect to="/404" />
+            </Switch>
+        </Router>
+    </ConfigProvider>,
     document.getElementById('root')
 );
 
